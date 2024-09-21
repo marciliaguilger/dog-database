f6594a4b3b4bb52b1573d7585adf73b174300c6d
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-21 11:30:31 -0300

    create RDS database

---
+++ database/main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "pos"
+}
+
+data "aws_secretsmanager_secret" "rds_postgres_credentials" {
+  name = "rds/postgres/dog-restaurant/credentials"
+}
+
+data "aws_secretsmanager_secret_version" "rds_postgres_credentials" {
+  secret_id = data.aws_secretsmanager_secret.rds_postgres_credentials.id
+}
+
+locals {
+  db_credentials = jsondecode(data.aws_secretsmanager_secret_version.rds_postgres_credentials.secret_string)
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-postgres-sg"
+  description = "Allow PostgreSQL access"
+  vpc_id      = "vpc-0b631c226edb3000c" 
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-postgres-sg"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "16.3"
+  instance_class       = "db.t3.micro"
+  db_name              = "dogrestaurantdb"
+  username             = local.db_credentials.username
+  password             = local.db_credentials.password
+  parameter_group_name = "postgres163"
+  skip_final_snapshot  = true
+
+  # Associa o Security Group à instância do RDS
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  publicly_accessible  = true
+  backup_retention_period = 0
+
+  tags = {
+    Name = "DogRestaurantInstance"
+  }
+}
